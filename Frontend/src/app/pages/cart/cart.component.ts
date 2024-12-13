d445721b34b8d891db3aa70f9122ce97663dd753
josie-opondo <josephineopondo5@gmail.com>
2024-12-14 01:15:58 +0300

    feat: add order summary directive to cart Component

--- Frontend/src/app/pages/cart/cart.component.ts
+++ Frontend/src/app/pages/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {CartService} from '../../services/cart.service';
 import {CartItemComponent} from './cart-item/cart-item.component';
+import {OrderSummaryComponent} from './order-summary/order-summary.component';
 
 
 @Component({
@@ -8,6 +9,7 @@ import {CartItemComponent} from './cart-item/cart-item.component';
   standalone: true,
   imports: [
     CartItemComponent,
+    OrderSummaryComponent,
   ],
   templateUrl: './cart.component.html',
   styles: ``
