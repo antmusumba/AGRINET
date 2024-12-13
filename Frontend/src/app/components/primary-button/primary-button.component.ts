108cdfe9e0966110f6e632c41ee7e68bc5f9d234
josie-opondo <josephineopondo5@gmail.com>
2024-12-14 00:18:52 +0300

    feat: adapt button styling to the current usage

--- Frontend/src/app/components/primary-button/primary-button.component.ts
+++ Frontend/src/app/components/primary-button/primary-button.component.ts
@@ -1,11 +1,14 @@
-import {Component, input} from '@angular/core';
+import {Component, input, output} from '@angular/core';
 
 @Component({
   selector: 'app-primary-button',
+  standalone: true,
   imports: [],
   templateUrl: './primary-button.component.html',
   styles: ``
 })
 export class PrimaryButtonComponent {
   label = input('')
+
+  btnClicked = output()
 }
