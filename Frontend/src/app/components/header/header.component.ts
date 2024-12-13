3dbb73f3bffad558b433c0f118e4d78568f48c61
josie-opondo <josephineopondo5@gmail.com>
2024-12-14 00:55:33 +0300

    fix: move cart button to header for later manipulation

--- Frontend/src/app/components/header/header.component.ts
+++ Frontend/src/app/components/header/header.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {NavComponent} from './nav/nav.component';
 import {RouterLink} from '@angular/router';
+import {CartService} from '../../services/cart.service';
+import {PrimaryButtonComponent} from '../primary-button/primary-button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     NavComponent,
-    RouterLink
+    RouterLink,
+    PrimaryButtonComponent
   ],
   templateUrl: './header.component.html',
   styles: ``
 })
 export class HeaderComponent {
+  cartService = inject(CartService);
 
+  showBtnClicked() {
+    console.log('showBtnClicked');
+  }
 }
