73212c24ca27c4cf54ed79964dd5bac5dd4bfc1a
josie-opondo <josephineopondo5@gmail.com>
2024-12-14 00:20:38 +0300

    chore: add @angular material configuration

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './components/header/header.component';
+import {HomeComponent} from './components/home/home.component';
+import {AuthComponent} from './pages/auth/auth.component';
+import {ProductsListComponent} from './pages/products-list/products-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, AuthComponent, ProductsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+
+  showAuthComponent : boolean = false;
+
+  onGetStarted(){
+    this.showAuthComponent = true;
+  }
 }
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import {ProductsListComponent} from './pages/products-list/products-list.component';
+import {AuthComponent} from './pages/auth/auth.component';
+import {HomeComponent} from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  component: HomeComponent,
+},
+  {
+    path: 'auth',
+    component: AuthComponent,
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+  },
+  {
+    path: 'product',
+    component: ProductsListComponent,
+  }
+];
