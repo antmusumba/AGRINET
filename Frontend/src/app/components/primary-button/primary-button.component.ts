caf219a8dabd9d2e65cc81b14fe5e12f49201414
josie-opondo <josephineopondo5@gmail.com>
2024-12-13 17:23:59 +0300

    feat: add reusable primary button

---
+++ Frontend/src/app/components/primary-button/primary-button.component.ts
@@ -0,0 +1,11 @@
+import {Component, input} from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  imports: [],
+  templateUrl: './primary-button.component.html',
+  styles: ``
+})
+export class PrimaryButtonComponent {
+  label = input('')
+}
