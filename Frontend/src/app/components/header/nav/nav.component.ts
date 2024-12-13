8166f77523a550179c9063c5c789123b3e27165b
josie-opondo <josephineopondo5@gmail.com>
2024-12-13 17:23:04 +0300

    feat: add nav componentfor sign in/ sign up navigation

---
+++ Frontend/src/app/components/header/nav/nav.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {PrimaryButtonComponent} from '../../primary-button/primary-button.component';
+
+@Component({
+  selector: 'app-nav',
+  imports: [
+    PrimaryButtonComponent
+  ],
+  templateUrl: './nav.component.html',
+  styles: ``
+})
+export class NavComponent {
+
+}
