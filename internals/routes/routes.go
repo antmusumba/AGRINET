b2a300d8549d8fcca61cba2ecc403f25f228acce
adiozdaniel <adiozdaniel@gmail.com>
2024-12-14 10:46:13 +0300

    feat: implement listing of products - to allow product catalogues - for user experience

--- internals/routes/routes.go
+++ internals/routes/routes.go
@@ -34,9 +34,10 @@ func (r *Router) SetupRoutes() http.Handler {
 	r.muxRouter.HandleFunc("/api/auth/login", r.handler.Login).Methods("POST")
 
 	r.muxRouter.HandleFunc("/api/products", r.handler.CreateProduct).Methods("POST")
+	r.muxRouter.HandleFunc("/api/products", r.handler.ListProducts).Methods("GET")
 
 	// TODO: Add product routes
-	// r.muxRouter.HandleFunc("/api/products/{id}", r.handler.GetProduct).Methods("GET")
+	//
 	// r.muxRouter.HandleFunc("/api/products", r.handler.GetProducts).Methods("GET")
 	// r.muxRouter.HandleFunc("/api/products/{id}", r.handler.UpdateProduct).Methods("PUT")
 	// r.muxRouter.HandleFunc("/api/products/{id}", r.handler.DeleteProduct).Methods("DELETE")
