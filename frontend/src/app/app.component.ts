edec795aeaa3180828f79bb4e31f79348b648379
adiozdaniel <adiozdaniel@gmail.com>
2024-12-14 20:33:41 +0300

    refactor: remove unnecessary imports - for a modular function

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/home/home.component';
-import { AuthComponent } from './pages/auth/auth.component';
-import { ProductsListComponent } from './pages/products-list/products-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    HomeComponent,
-    AuthComponent,
-    ProductsListComponent,
-  ],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
