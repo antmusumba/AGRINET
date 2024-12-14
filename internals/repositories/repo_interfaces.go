21a774db4bad00abfd312c29b8b89e9120c6c780
adiozdaniel <adiozdaniel@gmail.com>
2024-12-14 06:47:00 +0300

    chores: add a new member - List products will be able to return all available products

--- internals/repositories/repo_interfaces.go
+++ internals/repositories/repo_interfaces.go
@@ -2,6 +2,7 @@ package repositories
 
 import "github.com/antmusumba/agrinet/internals/models"
 
+// UserRepo represents the user repository
 type UserRepo interface {
 	CreateUser(user *models.User) error
 	GetUserByEmail(email string) (*models.User, error)
@@ -10,10 +11,12 @@ type UserRepo interface {
 	DeleteUser(id string) error
 }
 
+// ProductRepo represents the product repository
 type ProductRepo interface {
 	CreateProd(product *models.Product) error
 	GetPrdByEmail(email string) (*models.Product, error)
 	GetProdByID(id string) (*models.Product, error)
 	UpdateProd(product *models.Product) error
 	DeleteProd(id string) error
+	ListProducts() ([]*models.Product, error)
 }
