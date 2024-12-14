f8650233393812bb104d7c705babf5d51b29ed70
adiozdaniel <adiozdaniel@gmail.com>
2024-12-14 22:07:51 +0300

    fix: empty the dummy data - to align with the backend dynamic real data

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -1,27 +1,11 @@
 import { Injectable, signal } from '@angular/core';
-import {Product} from '../models/products.models';
+import { Product } from '../models/products.models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-
-  cart = signal<Product[]>([
-    {
-      id: 2,
-      title: "ihyfrdrtdytr",
-      price: 109.95,
-      image: "",
-      stock: 10,
-    },
-    {
-      id: 3,
-      title: "iouywertyuio",
-      price: 109.95,
-      image: "https://image.tmdb.org/t/p/w500",
-      stock: 10,
-    },
-  ]);
+  cart = signal<Product[]>([]);
 
   addToCart(product: Product) {
     this.cart.set([...this.cart(), product]);
@@ -31,5 +15,5 @@ export class CartService {
     this.cart.set(this.cart().filter((item) => item.id !== product.id));
   }
 
-  constructor() { }
+  constructor() {}
 }
