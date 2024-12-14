40ad315d8a624061f2a2cdae1362f2ab1b5fa976
adiozdaniel <adiozdaniel@gmail.com>
2024-12-14 23:59:35 +0300

    chores: subscribe to product service - to be able to pass information to and from the service

--- frontend/src/app/pages/products/product-form/product-form.component.ts
+++ frontend/src/app/pages/products/product-form/product-form.component.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
@@ -10,24 +9,32 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductsService } from '../../../services/products.service';
+import { Product } from '../../../models/products.models';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.css'],
   imports: [
-    CommonModule,
-    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatCardModule,
+    ReactiveFormsModule,
+    CommonModule,
   ],
 })
 export class AddProductComponent implements OnInit {
   addProductForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private productService: ProductsService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.addProductForm = this.fb.group({
@@ -41,8 +48,16 @@ export class AddProductComponent implements OnInit {
 
   onAddProduct(): void {
     if (this.addProductForm.valid) {
-      const newProduct = this.addProductForm.value;
-      console.log('Product Added:', newProduct);
+      const newProduct: Product = this.addProductForm.value;
+      this.productService.createProduct(newProduct).subscribe({
+        next: (addedProduct) => {
+          console.log('Product added successfully:', addedProduct);
+          this.router.navigate(['/products']);
+        },
+        error: (err) => {
+          console.error('Error adding product:', err);
+        },
+      });
     }
   }
 }
