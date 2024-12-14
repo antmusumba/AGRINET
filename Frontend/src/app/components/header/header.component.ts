d485a5c5f12105f579b8a418cce36114e2712d8f
josie-opondo <josephineopondo5@gmail.com>
2024-12-14 14:29:15 +0300

    fix: add directives for conditional display of buttons

--- Frontend/src/app/components/header/header.component.ts
+++ Frontend/src/app/components/header/header.component.ts
@@ -1,23 +1,43 @@
 import {Component, inject} from '@angular/core';
 import {NavComponent} from './nav/nav.component';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {CartService} from '../../services/cart.service';
 import {PrimaryButtonComponent} from '../primary-button/primary-button.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    NavComponent,
     RouterLink,
-    PrimaryButtonComponent
+    PrimaryButtonComponent,
+    NgIf
   ],
   templateUrl: './header.component.html',
   styles: ``
 })
 export class HeaderComponent {
+  constructor(private router: Router) {
+  }
   cartService = inject(CartService);
 
+  isLandingPage(): boolean {
+    return this.router.url === '/';
+  }
+
+  isProductsPage(): boolean {
+    return this.router.url === '/product';
+  }
+
+  isCartCheckout(): boolean {
+    return this.router.url === '/cart';
+  }
+
+  onSignOut() {
+    console.log('Signing out...');
+    // Add your sign-out logic (e.g., clear tokens, redirect)
+    this.router.navigate(['/auth']);
+  }
   showBtnClicked() {
     console.log('showBtnClicked');
   }
