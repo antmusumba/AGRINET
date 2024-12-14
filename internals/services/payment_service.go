da4465b5543f23dd60ab55f3eea57c8ac110db53
adiozdaniel <adiozdaniel@gmail.com>
2024-12-14 19:08:15 +0300

    feat: incorporate payment service

---
+++ internals/services/payment_service.go
@@ -0,0 +1,57 @@
+package services
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+// Fake credentials
+// TODO: replace with real credentials and store in environment variables
+const (
+	password  = "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjQxMjE0MTgyOTQ0"
+	ShortCode = 174379
+)
+
+// ProcessStkPush processes a STK push payment
+func ProcessStkPush(phoneNumber string, amount int) (string, error) {
+	url := "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+	method := "POST"
+	payload := strings.NewReader(fmt.Sprintf(`{
+    "BusinessShortCode": %d,
+    "Password": %s,
+    "Timestamp": "20241214182944",
+    "TransactionType": "CustomerPayBillOnline",
+    "Amount": 1,
+    "PartyA": 254708374149,
+    "PartyB": 174379,
+    "PhoneNumber": 254720804060,
+    "CallBackURL": "https://mydomain.com/path",
+    "AccountReference": "CompanyXLTD",
+    "TransactionDesc": "Payment of X"
+  }`, ShortCode, password))
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		return "", fmt.Errorf("error creating request: %v", err)
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer Odn8HiE1c2JeVjqWt1oh7OByDB4a")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error making request: %v", err)
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response body: %v", err)
+	}
+
+	return string(body), nil
+}
